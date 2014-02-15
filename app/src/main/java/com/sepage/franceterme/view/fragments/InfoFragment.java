e1252387d1c760b10b93a919b6ec9c5516433c2c
Dana Kianfar <dana.kianfar@gmail.com>
2014-02-15 21:11:36 +0100

    added correct text for textviews

--- app/src/main/java/com/sepage/franceterme/view/fragments/InfoFragment.java
+++ app/src/main/java/com/sepage/franceterme/view/fragments/InfoFragment.java
@@ -2,15 +2,14 @@ package com.sepage.franceterme.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.sepage.franceterme.R;
 
-/**
- * Created by Balrog on 1/10/14.
- */
 public class InfoFragment extends Fragment {
 
     View view;
@@ -29,6 +28,10 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater,container,savedInstanceState);
         view = inflater.inflate(R.layout.info_fragment, container, false);
+
+        TextView infoPage = (TextView) view.findViewById(R.id.infoPageTextView);            // set HTML
+        infoPage.setText(Html.fromHtml(getResources().getString(R.string.info_page_text)));
+
         return view;
     }
 
